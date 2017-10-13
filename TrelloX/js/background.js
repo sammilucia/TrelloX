7b37843ea75d391e8950ccef0aaa6babd0d3b084
Samantha Gilbert <3295286+sammilucia@users.noreply.github.com>
2017-10-13 15:58:42 +1030

    remove script injection

--- TrelloX/js/background.js
+++ TrelloX/js/background.js
@@ -1,9 +1,5 @@
 var injected, currentUrl;
 
-function inject() {
-    chrome.tabs.executeScript(null,{file:"script.js"});
-}
-
 chrome.webNavigation.onCompleted.addListener(function(details) {
     inject();
 });
